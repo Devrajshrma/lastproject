abdc4904c732084db7b3b44abc74189fac91b3be
Devrajshrma <devrajsharma0804@gmail.com>
2024-09-27 00:04:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// typing js for attarctive my hero section
+document.addEventListener("DOMContentLoaded", function () {
+  var typed = new Typed('#element', {
+    strings: ['UI Developer', 'Frontend Developer'],
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 1000,
+    startDelay: 500,
+    loop: true
+  });
+});
